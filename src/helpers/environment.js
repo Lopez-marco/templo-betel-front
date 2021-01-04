5796497c2d05ddcbef29d8cb2849ae97f02c288c
Marco Lopez <lopez.marcohdez@gmail.com>
2021-01-04 09:26:44 -0500

    image fix?

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'templobetel.us':
+    case 'templo-betel-front.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://templo-betel-back.herokuapp.com'
 }
