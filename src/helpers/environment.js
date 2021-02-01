cf29916ffbca6ea0f769a7f67c2a330ffa4027b5
Marco Lopez <lopez.marcohdez@gmail.com>
2021-02-01 10:50:31 -0500

    domain fix

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'templobetel.us':
+    case 'templo-betel-client.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://templo-betel-server.herokuapp.com'
 }
